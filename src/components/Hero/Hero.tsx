6cf6b9197c4e17f9c5541781efa208f4de10e740
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-08-01 02:24:51 +0900

    add: PC Styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,26 @@
+'use client';
+
 import React from "react";
-import HeroMainCopy from "./HeroMainCopy";
-import HeroSubCopy from "./HeroSubCopy";
+import HeroMainCopy from "./HeroMainCopy/HeroMainCopy";
+import HeroMainCopyPc from "./HeroMainCopy/HeroMainCopyPc";
+import HeroSubCopy from "./HeroSubCopy/HeroSubCopy";
 import HeroBackLogo from "./HeroBackLogo";
 import ScrollDown from "./ScrollDown/ScrollDown";
 import styles from "./Hero.module.css";
+import useIsPc from "@/hooks/useIsPc";
+import HeroSubCopyPc from "./HeroSubCopy/HeroSubCopyPc";
 
 export default function Hero() {
+  const isPc = useIsPc();
+
   return (
     <div className={styles.Hero}>
       <div className={styles.Hero__Copy}>
         <div className={styles.Hero__MainCopy}>
-          <HeroMainCopy />
-        </div>  
+          { isPc ? <HeroMainCopyPc /> : <HeroMainCopy /> }
+        </div>
         <div className={styles.Hero__SubCopy}>
-          <HeroSubCopy />
+          { isPc ?<HeroSubCopyPc /> : <HeroSubCopy /> }
         </div>
       </div>
       <div className={styles.Hero__BackLogo}>
