492e969dbdbf740db3ae6a38febbc33c055a8f34
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-12-13 23:20:55 +0900

    feat: スクロール時にカクカクするため、フッターの角丸をCSS完結できる形に変更

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 
 import ContactButton from '../ContactButton/ContactButton';
-import Corner, { CornerPosition } from '../Corner/Corner';
 
 import styles from './Footer.module.css';
 import FooterMenu from './FooterMenu/FooterMenu';
 import Pagetop from './Pagetop/Pagetop';
 
-
 export default function Footer() {
+  
   return (
     <footer className={styles.Footer}>
-      <div className={styles.Footer__Inner}>
-        <FooterMenu />
-        <div className={styles.Footer__ContactButton}>
-          <ContactButton modifier='isFooter' />
-        </div>
-        <div className={styles.Footer__Pagetop}>
-          <Pagetop />
+      <div className={styles.Footer__Container}>
+        <div className={styles.Footer__Inner}>
+          <FooterMenu />
+          <div className={styles.Footer__ContactButton}>
+            <ContactButton modifier="isFooter" />
+          </div>
+          <div className={styles.Footer__Pagetop}>
+            <Pagetop />
+          </div>
         </div>
       </div>
-      <Corner bottom='-60px' left="8px" position={CornerPosition.BOTTOM_LEFT} />
-      <Corner bottom='-60px' right="8px" position={CornerPosition.BOTTOM_RIGHT} />
     </footer>
   );
 }
