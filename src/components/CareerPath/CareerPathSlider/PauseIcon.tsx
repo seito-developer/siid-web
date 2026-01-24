9320a633adc4ef58cb96c96515831a30024d495f
yogo <8400185+k-yogo@users.noreply.github.com>
2026-01-25 06:29:04 +0900

    chore: Remove unused PauseIcon and PlayIcon components from CareerPathSlider to streamline codebase

--- src/components/CareerPath/CareerPathSlider/PauseIcon.tsx
+++
@@ -1,8 +0,0 @@
-export default function PauseIcon({ className }: { className?: string }) {
-  return (
-    <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-      <path d="M5.375 0.75L5.375 9.1875" stroke="#342525" strokeWidth="1.5" strokeLinecap="round" />
-      <path d="M0.75 0.75L0.75 9.1875" stroke="#342525" strokeWidth="1.5" strokeLinecap="round" />
-    </svg>
-  );
-}
