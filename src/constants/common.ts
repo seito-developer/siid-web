1693e487a950cf059d286e844b84a1c92d609778
yogo <8400185+k-yogo@users.noreply.github.com>
2026-01-27 07:47:52 +0900

    style: Update font weights across multiple components for improved visual hierarchy and consistency

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -4,10 +4,10 @@ export const BREAK_POINT = 1280;
 
 export const notoSansJp = Noto_Sans_JP({
   variable: '--font-noto-sans-jp',
-  weight: ['400', '500', '600', '900'],
+  weight: ['400', '900'],
 });
 
 export const poppins = Poppins({
   variable: '--font-poppins',
-  weight: ['400', '600', '700', '800', '900'],
+  weight: ['400', '900'],
 });
