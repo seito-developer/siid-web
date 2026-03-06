36db496033fd5e1b1f469f7445d303f917133745
Seito Horiguchi <seito.horiguchi@gmail.com>
2026-03-07 01:56:22 +0900

    fix: footer animation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+'use client';
 
+import useIsPc from '../../hooks/useIsPc';
 import ContactButton from '../ContactButton/ContactButton';
+import Corner, { CornerPosition } from '../Corner/Corner';
 
 import styles from './Footer.module.css';
 import FooterMenu from './FooterMenu/FooterMenu';
 import Pagetop from './Pagetop/Pagetop';
 
 export default function Footer() {
-  
+  const isPc = useIsPc();
+
   return (
     <footer className={styles.Footer}>
+      {isPc && (
+        <>
+          <Corner
+            width="16px"
+            height="16px"
+            bottom="0"
+            left="8px"
+            position={CornerPosition.BOTTOM_LEFT}
+          />
+          <Corner
+            width="16px"
+            height="16px"
+            bottom="0"
+            right="8px"
+            position={CornerPosition.BOTTOM_RIGHT}
+          />
+        </> )}
+
       <div className={styles.Footer__Container}>
         <div className={styles.Footer__Inner}>
           <FooterMenu />
